350141f53a012ccd453b21e8e3ddfeb249952a16
Arpit <arpit27kr@gmail.com>
2023-02-20 00:16:58 +0530

    Add files via upload

---
+++ Patterns/Character_pattern.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+public class Character_pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char ch ='A';
+		for(int i=1;i<5; i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(ch);
+				ch++;
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
