2c20a7c43a94b4dbb14b69fe14ef4a2dfe054027
Arpit <arpit27kr@gmail.com>
2023-02-24 21:13:51 +0530

    Add files via upload

---
+++ Patterns/Half_pyramid.java
@@ -0,0 +1,24 @@
+package Patterns;
+
+public class Half_pyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int k=1;
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				int count=j;
+				System.out.print(count);
+				count++;
+				
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
